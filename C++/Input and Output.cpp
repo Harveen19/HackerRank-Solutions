d6f4c0623531f32383138a215863bb9a76ef99a0
Harveen19 <harveenkaur1912@gmail.com>
2023-01-04 14:22:38 +0530

    C++ Solutions

---
+++ C++/Input and Output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+       int a=1;
+       int b=2;
+       int c=7;
+       
+       cin>>a>>b>>c;
+       int sum=a+b+c;
+       
+    cout<< sum;
+    return 0;
+}
