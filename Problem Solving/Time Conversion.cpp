90c76f5b6ead3af0e0da8868cfe8359060734d38
Harveen Kaur <78599929+Harveen19@users.noreply.github.com>
2023-01-14 18:39:39 +0530

    Problem Solving Solutions

---
+++ Problem Solving/Time Conversion.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+int main()
+{
+    string s;
+    cin >> s;
+    
+    int n = s.length();
+    int hh, mm, ss;
+    
+    hh = (s[0] - '0') * 10 + (s[1] - '0');
+    mm = (s[3] - '0') * 10 + (s[4] - '0');
+    ss = (s[6] - '0') * 10 + (s[7] - '0');
+    
+    if (hh < 12 && s[8] == 'P') hh += 12;
+    if (hh == 12 && s[8] == 'A') hh = 0;
+    printf("%02d:%02d:%02d\n", hh, mm, ss);
+
+    return 0;
+}
